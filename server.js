a92fe62e050f416cd6df3b236e1635c21e489b7f
Cam Pedersen <diffference@gmail.com>
2013-01-18 01:56:33 -0600

    allow CORS and actually parse bodies

--- server.js
+++ server.js
@@ -10,6 +10,17 @@ program
 
 var express = require('express');
 var app = process.app = module.exports = express();
+
+var allowCrossDomain = function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'x-requested-with,content-type');
+  next();
+}
+
+app.use(allowCrossDomain);
+app.use(express.bodyParser());
+
 require('./routes');
 
 if (!module.parent) {
