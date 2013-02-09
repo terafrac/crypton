b9ed2a441c16a5ead7a3f1a046d9026ff02ef25b
Cam Pedersen <diffference@gmail.com>
2013-02-09 00:45:41 +0000

    further expand storage example

--- client/src/container.js
+++ client/src/container.js
@@ -2,22 +2,39 @@
   var Container = crypton.Container = function () {
     this.keys = {};
     this.versions = {};
+    this.version = 0;
   }
 
   Container.prototype.add = function (key, value) {
+    if (this.keys[key]) {
+      callback('Key already exists');
+      return;
+    }
 
+    this.keys[key] = {};
   }
 
   Container.prototype.get = function (key, callback) {
+    if (!this.keys[key]) {
+      callback('Key does not exist');
+      return;
+    }
+
+    callback(null, this.keys[key]);
   }
 
   Container.prototype.save = function (callback) {
+    callback();
   }
 
   Container.prototype.getHistory = function (callback) {
+
   }
 
   Container.prototype.getDiff = function (callback) {
+    for (var i in this.keys) {
+
+    }
   }
 })();
 
