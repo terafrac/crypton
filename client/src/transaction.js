d5581b68649a1859dfe046d70c098585e41ed5cb
Cam Pedersen <diffference@gmail.com>
2013-02-12 20:11:33 +0000

    expand transaction functionality

--- client/src/transaction.js
+++ client/src/transaction.js
@@ -1,21 +1,87 @@
 (function () {
   var Transaction = crypton.Transaction = function () {
     this.chunks = [];
-    this.id = null;
+    this.create(function (err, token) {
+      if (err) {
+        console.log(err);
+      }
 
-    // call the server to get a transaction id
-  }
+      this.token = token;
+    }.bind(this));
+  };
+
+  Transaction.prototype.create = function (callback) {
+    var url = crypton.url() + '/transaction/create';
+    superagent.post(url).end(function (res) {
+      if (!res.body || res.body.success !== true) {
+        callback(res.body.error);
+        return;
+      }
+
+      callback(null, res.body.token);
+    });
+  };
 
   // create diffs of container and add to chunks array
   Transaction.prototype.save = function () {
+    this.verify();
     for (var i in arguments) {
       console.log(i + ': ' + arguments[i]);
-      this.chunks.push();
+      var chunk = arguments[i];
+      this.chunks.push(chunk);
+      this.saveChunk(chunk, function () {
+        console.log(arguments);
+      });
     }
-  }
+  };
+
+  Transaction.prototype.saveChunk = function (chunk, callback) {
+    this.verify();
+    var url = crypton.url() + '/transaction/create';
+    superagent.post(url)
+      .send(chunk)
+      .end(function (res) {
+        console.log(res.body);
+        if (!res.body || res.body.success !== true) {
+          callback(res.body.error);
+          return;
+        }
+
+        callback();
+      });
+  };
 
   // push chunks to the server
   Transaction.prototype.commit = function () {
-  }
+    this.verify();
+    var url = crypton.url() + '/transaction/' + this.token + '/commit';
+    superagent.post(url).end(function (res) {
+      if (!res.body || res.body.success !== true) {
+        callback(res.body.error);
+        return;
+      }
+
+      callback();
+    });
+  };
+
+  Transaction.prototype.cancel = function (callback) {
+    this.verify();
+    var url = crypton.url() + '/transaction/' + this.token;
+    superagent.del(url).end(function (res) {
+      if (!res.body || res.body.success !== true) {
+        callback(res.body.error);
+        return;
+      }
+
+      callback();
+    });
+  };
+
+  Transaction.prototype.verify = function () {
+    if (!this.token) {
+      throw new Error('Invalid transaction');
+    }
+  };
 })();
 
