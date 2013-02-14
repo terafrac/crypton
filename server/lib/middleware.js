37c1256a804050c81419fd6bd6f045eabab93a87
Cam Pedersen <diffference@gmail.com>
2013-02-14 21:18:21 +0000

    session middleware correctly reconstructs session

--- server/lib/middleware.js
+++ server/lib/middleware.js
@@ -2,15 +2,22 @@ var middleware = module.exports = {};
 
 middleware.verifySession = function (req, res, next) {
   var id = req.headers['session-identifier'];
-  req.session = req.sessionStore.sessions[id]; // can we exploit this?
 
-  if (req.session.accountId) {
-    next();
-    return;
-  }
-
-  res.send({
-    success: false,
-    error: 'Invalid session'
+  req.sessionStore.get(id, function (err, session) {
+    if (!err && session.accountId) {
+      // TODO this may be a leak but it's the only
+      // way to get around CORS without implementing our
+      // own sessionStore
+      Object.keys(session).forEach(function (i) {
+        if (i == 'cookie') return;
+        req.session[i] = session[i];
+      });
+      next();
+    } else {
+      res.send({
+        success: false,
+        error: 'Invalid session'
+      });
+    }
   });
 };
