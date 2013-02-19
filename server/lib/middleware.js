4a353a9b3ae2a74cc79701ca2a710f84fe1b1c81
Cam Pedersen <diffference@gmail.com>
2013-02-19 23:21:55 +0000

    refactor session middleware

--- server/lib/middleware.js
+++ server/lib/middleware.js
@@ -4,20 +4,22 @@ middleware.verifySession = function (req, res, next) {
   var id = req.headers['session-identifier'];
 
   req.sessionStore.get(id, function (err, session) {
-    if (!err && session.accountId) {
-      // TODO this may be a leak but it's the only
-      // way to get around CORS without implementing our
-      // own sessionStore
-      Object.keys(session).forEach(function (i) {
-        if (i == 'cookie') return;
-        req.session[i] = session[i];
-      });
-      next();
-    } else {
+    if (err || !session || !session.accountId) {
       res.send({
         success: false,
         error: 'Invalid session'
       });
+      return;
     }
+
+    // TODO this may be a leak but it's the only
+    // way to get around CORS without implementing our
+    // own sessionStore
+    Object.keys(session).forEach(function (i) {
+      if (i == 'cookie') return;
+      req.session[i] = session[i];
+    });
+
+    next();
   });
 };
