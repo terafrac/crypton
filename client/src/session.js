f422cf2bfece265b3511667792de558a20ae0ecd
Bryon Roché <kain@kain.org>
2013-02-25 00:20:33 -0800

    identify RSA encoding issues for next pass

--- client/src/session.js
+++ client/src/session.js
@@ -41,9 +41,16 @@
 
     var sessionKey = crypton.randomBytes(32);
     var hmacKey = crypton.randomBytes(32);
-    var signature = 'hello'; // TODO sign with private key
     var sessionKeyCiphertext = this.account.keypair.encrypt(sessionKey.toString());
     var hmacKeyCiphertext = this.account.keypair.encrypt(hmacKey.toString());
+    var keyshmac = CryptoJS.HmacSHA256(sessionKey.toString() + hmacKey.toString(), '')
+    // > v = String.fromCharCode.apply(String, new Uint8Array(new Uint32Array(c.CryptoJS.SHA512('moo').words).buffer))
+    // this would be the way to get a binary hash out, but we need to figure out how we're doing data marshalling in
+    // this whole ball of wax.  Alan says that we should be working on binary data as much as possible. all the RSA stuff is
+    // implicitly stringencoding.  CryptoJS gives the option not to, but we probably need to factor all the crypto stuff to separate
+    // crypto from encoding.  I'm going to leave this as is, with the example code above.  I'm also pretty confident that rsasign.js should work, but
+    // we need to patch/refactor pretty much everything under rsa/* to not pervasively toString() everything.
+    var signature = 'hello'; // TODO sign with private key
     var containerNameHmac = CryptoJS.HmacSHA256(
       containerName,
       this.account.containerNameHmacKey
